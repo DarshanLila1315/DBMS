8bdaf35b390bb7cf09de797d566091cac0f48b88
Darshan Lila <23020201093@darshan.ac.in>
2026-08-07 08:56:20 +0530

    Refactor SQL commands for DEPOSIT and STUDENT tables

--- LAB - 05.sql
+++ LAB - 05.sql
@@ -39,31 +39,98 @@ SP_RENAME 'DEPOSIT.ACTON' ,'ANO'
 SELECT * FROM DEPOSIT
 
 -- 7. Rename column bname to branch_name.
+SP_RENAME 'DEPOSIT.BNAME','BRANCH_NAME'
+SELECT * FROM DEPOSIT
 
--- 8. Rename table DEPOSIT to DEPOSIT_DETAIL.  
+
+
+-- 8. Rename table DEPOSIT to DEPOSIT_DETAIL. 
+SP_RENAME 'DEPOSIT','DEPOSIT_DETAIL'
+SELECT * FROM DEPOSIT_DETAIL
+
+
 -- 9. Add column ifsc_code varchar(15).  
+ALTER TABLE DEPOSIT_DETAIL
+ADD IFSC_CODE VARCHAR(15)
+SELECT * FROM DEPOSIT_DETAIL
 -- 10. Change the size of bname column from varchar(50) to varchar(30). 
+ALTER TABLE DEPOSIT_DETAIL
+ALTER COLUMN BRANCH_NAME VARCHAR(30)
+
  
 -- Part – B: 
 -- 11. Rename column adate to aopendate.  
+
+SP_RENAME 'DEPOSIT_DETAIL.ADATE', 'AOPNDATE'
+SELECT * FROM DEPOSIT_DETAIL
+
+
 -- 12. Delete column aopendate from DEPOSIT_DETAIL table.  
+
+ALTER TABLE DEPOSIT_DETAIL
+DROP COLUMN AOPNDATE
+SELECT * FROM DEPOSIT_DETAIL
+
 -- 13. Rename column cname to customer_name.  
+
+SP_RENAME 'DEPOSIT_DETAIL.[CNAME]', 'CUSTOMER_NAME'
+SELECT * FROM DEPOSIT_DETAIL
+
 -- 14. Add column country varchar(20).  
+
+ALTER TABLE DEPOSIT_DETAIL
+ADD COUNTRY VARCHAR(20)
+SELECT * FROM DEPOSIT_DETAIL
+
 -- 15. Add column account_type varchar(15). 
+
+ALTER TABLE DEPOSIT_DETAIL
+ADD ACCOUNT_TYPE VARCHAR(15)
+SELECT * FROM DEPOSIT_DETAIL
+
  
  
 -- Part – C: 
 -- 16. Change data type of pincode from int to bigint. 
+
+ ALTER TABLE DEPOSIT_DETAIL
+ALTER COLUMN PINCODE BIGINT
+SELECT * FROM DEPOSIT_DETAIL
+
 -- 17. Delete column account_type. 
+
+ALTER TABLE DEPOSIT_DETAIL
+DROP COLUMN ACCOUNT_TYPE
+SELECT * FROM DEPOSIT_DETAIL
+
 -- 18. Rename column amount to balance. 
+
+SP_RENAME 'DEPOSIT_DETAIL.[AMOUNT]', 'BALANCE'
+SELECT * FROM DEPOSIT_DETAIL
+
+
+
 -- 19. Add column status varchar(10). 
+
+ALTER TABLE DEPOSIT_DETAIL
+ADD STATUS VARCHAR(10)
+SELECT * FROM DEPOSIT_DETAIL
+
 -- 20. Change table name deposit_detail to bank_deposit. 
+
+SP_RENAME 'DEPOSIT_DETAIL', 'BANK_DEPOSIT'
+SELECT * FROM BANK_DEPOSIT
+
  
 
 -- From the table DEPOSIT perform the following queries:  
 
 -- Part – A: 
 -- 1. Delete all the records having amount less than or equal to 3000.  
+DELETE FROM BANK_DEPOSIT
+
+
+
 -- 2. Delete all the accounts of ‘BEDI’ branch customer. 
 -- 3. Delete all the accounts having account number greater than 102 and less than 109.  
 -- 4. Delete all the accounts whose branch is ‘BEDI’ or ‘MADHAPAR’.  
